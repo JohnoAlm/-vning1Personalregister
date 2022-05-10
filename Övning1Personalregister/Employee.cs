f0e8a381ce679d5c3fafc33f000c8583e4edd1f1
ollid <ollid@DESKTOP-FAEEGUE>
2022-05-10 16:46:20 +0200

    Add project files.

---
+++ Övning1Personalregister/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning1Personalregister
+{
+    internal class Employee
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+
+        //public Employee(string firstName, string lastName, int salary)
+        //{
+        //    FirstName = firstName;
+        //    LastName = lastName;
+        //    Salary = salary;
+        //}
+
+        public void DisplayEmployee()
+        {
+            Console.WriteLine("Namn: " + FirstName + " " + LastName + "\n" + "Lön: " + Salary + "kr");
+        }
+        
+
+    }
+}
---
+++ Övning1Personalregister/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Övning1Personalregister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 11; i++)
+            {
+            Employee employee = new Employee();
+            
+            
+            Console.WriteLine("Mata in förnamn:");
+            employee.FirstName = Console.ReadLine();
+            Console.WriteLine("Mata in efternamn");
+            employee.LastName = Console.ReadLine();
+            Console.WriteLine("Mata in lön:");
+            employee.Salary = Convert.ToInt32(Console.ReadLine());
+
+            employee.DisplayEmployee();
+            }
+
+                
+            
+
+            
+            
+
+            
+
+        }
+    }
+}
