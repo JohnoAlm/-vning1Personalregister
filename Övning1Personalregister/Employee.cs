ec79f0adb60a54f1e3e7c6bb3887ccf5c9aaf899
almenoliver02@gmail.com <almenoliver02@gmail.com>
2022-05-13 09:04:05 +0200

    V2

--- Övning1Personalregister/Employee.cs
+++ Övning1Personalregister/Employee.cs
@@ -8,21 +8,13 @@ namespace Övning1Personalregister
 {
     internal class Employee
     {
-
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Salary { get; set; }
-
-        //public Employee(string firstName, string lastName, int salary)
-        //{
-        //    FirstName = firstName;
-        //    LastName = lastName;
-        //    Salary = salary;
-        //}
-
+        
         public void DisplayEmployee()
         {
-            Console.WriteLine("Namn: " + FirstName + " " + LastName + "\n" + "Lön: " + Salary + "kr");
+            Console.WriteLine($"Namn: {FirstName} {LastName} \nLön: {Salary}kr");
         }
         
 
--- Övning1Personalregister/Program.cs
+++ Övning1Personalregister/Program.cs
@@ -6,21 +6,45 @@ namespace Övning1Personalregister
     {
         static void Main(string[] args)
         {
-            for(int i = 0; i < 11; i++)
-            {
-            Employee employee = new Employee();
-            
-            
-            Console.WriteLine("Mata in förnamn:");
-            employee.FirstName = Console.ReadLine();
-            Console.WriteLine("Mata in efternamn");
-            employee.LastName = Console.ReadLine();
-            Console.WriteLine("Mata in lön:");
-            employee.Salary = Convert.ToInt32(Console.ReadLine());
+            List<Employee> list = new List<Employee>();
+            string firstname, lastname;
+            int salary;
 
-            employee.DisplayEmployee();
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Mata in förnamn:");
+                firstname = Console.ReadLine();
+
+                Console.WriteLine("Mata in efternamn");
+                lastname = Console.ReadLine();
+
+                Console.WriteLine("Mata in lön:");
+                salary = Convert.ToInt32(Console.ReadLine());
+
+                list.Add(new Employee
+                {
+                    FirstName = firstname,
+                    LastName = lastname,
+                    Salary = salary
+                });
             }
 
+            Console.WriteLine("\n");
+            Console.WriteLine("Personalregister:");
+            Console.WriteLine("____________________");
+            Console.WriteLine("\n");
+
+
+            foreach(Employee employee in list)
+            {
+                employee.DisplayEmployee();
+            }
+
+            Console.ReadLine();
+
+
+            
                 
             
 
